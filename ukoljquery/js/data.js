731159cca02f8b20ebe94aa29a21a2aa5578211d
michalsemenisin <45061666+michalsemenisin@users.noreply.github.com>
2019-03-21 20:34:13 +0100

    Add files via upload

---
+++ ukoljquery/js/data.js
@@ -0,0 +1,14 @@
+var planety = [
+    {
+        "photo": "obrazek3.png",
+        "popisek": "Vodní exoplaneta"
+    },
+    {
+        "photo": "obrazek2.png",
+        "popisek": "Kepler-1647b"
+    },
+    {
+        "photo": "obrazek1.png",
+        "popisek": "TrES-4"
+    }
+];
